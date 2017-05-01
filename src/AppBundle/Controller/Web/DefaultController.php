c171c53453d7d0e841261cdcf6f775578262da8e
petrero <petrero@gmail.com>
2017-05-01 14:43:29 +0300

    user: weaverryan and the very secure password foo       Copy code from Knp site.       Run:         composer install         bin/console doctrine:database:create         bin/console doctrine:database:create --env=test         bin/console doctrine:schema:create         bin/console doctrine:schema:create --env=test         bin/console doctrine:fixtures       Generate jwt keys by running:         openssl genrsa -out var/jwt/private.pem -aes256 4096         openssl rsa -pubout -in var/jwt/private.pem -out var/jwt/public.pem

---
+++ src/AppBundle/Controller/Web/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller\Web;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Controller\BaseController;
+
+class DefaultController extends BaseController{
+  /**
+   * @Route("/", name="homepage")
+   */
+  public function homepageAction(){
+    return $this->render('homepage.twig');
+  }
+}
