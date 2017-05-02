4cc3c85f50c51659be3f5bc173407c6f51f3bdfe
petrero <petrero@gmail.com>
2017-05-02 19:18:40 +0300

    Copy code from branch 2

---
+++ src/AppBundle/Tests/Controller/Api/BattleControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace AppBundle\Tests\Controller\Api;
+
+use AppBundle\Test\ApiTestCase;
+
+class BattleControllerTest extends ApiTestCase {
+  protected function setUp(){
+    parent::setUp();
+    $this->createUser('weaverryan');
+  }
+  
+  public function testPOSTCreateBattle(){
+    $project = $this->createProject('my_project');
+    $programmer = $this->createProgrammer([
+      'nickname' => 'Fred'
+    ], 'weaverryan');
+    
+    $data = array(
+      'project'    => $project->getId(),
+      'programmer' => $programmer->getId()
+    );
+    
+    $response = $this->client->post('/api/battles', [
+      'body'    => json_encode($data),
+      'headers' => $this->getAuthorizedHeaders('weaverryan')
+    ]);
+    $this->assertEquals(201, $response->getStatusCode());
+    $this->asserter()->assertResponsePropertyExists($response, 'didProgrammerWin');
+    //to do later
+    //$this->assertTrue($response->hasHeader('Location'))
+  }
+}
